bd60c0e3488ac94f83cd13de88bdfc121a4e4e5b
LucianaNascimento <luci.nasrika@hotmail.com>
2022-10-21 00:34:16 -0300

    home responsive

--- scripts.js
+++ scripts.js
@@ -1,3 +1,24 @@
 const logoLink = () => {
     window.location.href = "home.html";
 }
+
+const gerarCodigo = () => {  
+  let code = Math.floor(Date.now() * Math.random()).toString(36)
+  document.getElementById('generated-code').innerHTML = code
+}
+
+
+let input = ""
+let btn = document.querySelector("#ok-confirme-presence")
+
+btn.onclick = function() {
+ 
+  input = document.querySelector("#insert-code");
+  const texto = input.value;
+
+  console.log(texto)
+}
+
+
+
+
